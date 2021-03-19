11d9df66149a8a60a73acb7a8aa4eb7662675f88
Burrel Vann Jr <burrelvannjr@Burrels-Macbook-Pro-2019.local>
2021-03-18 19:04:08 -0700

    added r script for t-test

---
+++ resources/exercises/t-test.R
@@ -0,0 +1,58 @@
+#----------------------------------------
+# Load packages
+#----------------------------------------
+library(MASS)
+library(psych)
+devtools::install_github("burrelvannjr/vannstats") #run this to update package from github
+library(vannstats)
+
+#----------------------------------------
+# call mtcars data in console
+#----------------------------------------
+mtcars
+
+#----------------------------------------
+# load mtcars as new 'data1' object
+#----------------------------------------
+data1 <- mtcars
+
+#----------------------------------------
+# univariate stats for 'mpg' variable in 'data1' object, broken out by 'am' variable
+#----------------------------------------
+describeBy(data1$mpg, data1$am)
+
+#----------------------------------------
+# histogram of 'mpg' broken out by 'am'
+#----------------------------------------
+hst(data1, mpg, am)
+
+#----------------------------------------
+# boxplot of 'mpg' broken out by 'am'
+#----------------------------------------
+box(data1, mpg, am)
+
+#----------------------------------------
+# qq plot of 'mpg' broken out by 'am'
+#----------------------------------------
+qq(data1, mpg, am)
+
+#----------------------------------------
+# run the t-test, examining the difference between 'mpg' means for each 'am;
+#----------------------------------------
+t.test(data1$mpg ~ data1$am, var.equal=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
