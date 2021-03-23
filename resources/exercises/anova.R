88837c5b1de5bec1d45aa0bbc076555d7f39dedf
Burrel Vann Jr <burrelvannjr@Burrels-Macbook-Pro-2019.local>
2021-03-23 10:43:00 -0700

    cleaned code and anova page

--- resources/exercises/anova.R
+++ resources/exercises/anova.R
@@ -3,10 +3,8 @@
 #----------------------------------------
 library(MASS)
 library(psych)
-#install.packages("gplots") #run this to install the 'gplots' package for plotting
+install.packages("gplots") #run this to install the 'gplots' package for plotting
 library(gplots)
-#install.packages("stats") #run this to install the 'stats' package for running posthoc tests
-library(stats)
 devtools::install_github("burrelvannjr/vannstats") #run this to update package from github
 library(vannstats)
 
@@ -48,9 +46,9 @@ summary(aov(data1$mpg ~ data1$cyl))
 #----------------------------------------
 # plot the means (and 95% confidence intervals around the mean) to examine whether and which means are different from others
 #----------------------------------------
-plotmeans(mpg ~ cyl, data = data1, 
+plotmeans(mpg ~ cyl, data = data1,
           xlab = "Cylinder", ylab = "Miles Per Gallon",
-          main="Mean Plot with 95% CI") 
+          main="Mean Plot with 95% CI")
 
 #----------------------------------------
 # run Tukey HSD post-hoc test to examine whether and which means are *significantly* different from others. Exhaustive list of comparisons, adjusts alpha to account for all comparisons
